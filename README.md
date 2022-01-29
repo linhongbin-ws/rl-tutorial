1c49e2d727d71ab998051e97f11a2f5d6f12f079
Ben <522181941@qq.com>
2022-01-29 21:41:15 +0800

    [add]

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rl-tutorials
+
+
+## Installation
+
+```sh
+conda create -n rl-tutorial python=3.7
+```
---
+++ rl_tutorial/01-DP/policy_evaluation.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pprint
+import sys
+from rl_tutorial.lib.envs.gridworld import GridworldEnv
